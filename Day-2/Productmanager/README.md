071cfb73eace2ab6d12421c0ca6ddb7b0c4a7db1
Bhanu-medianv <bhanupratapsingh@medianv>
2025-05-23 19:02:24 +0530

    KRA day-2

---
+++ Day-2/Productmanager/README.md
@@ -0,0 +1,17 @@
+Your Challenge ->
+ 
+Build a product management system that processes the provided data to generate a detailed analytics report for an online store.
+ 
+- Define ES6 classes for Product and Customer.
+- Instantiate objects from rawData.
+- Use ES6 features such as let/const, arrow functions, template literals, destructuring, array and string methods, object utilities, and Promises.
+- Calculate and display:
+  - Total products by category
+  - Average ratings per category
+  - Customers with the most orders
+  - Products needing restock (stock < threshold)
+  - Revenue analysis (total inventory value)
+- Output the results in the exact report format below.
+- Add clear console.log statements demonstrating your use of ES6 features.
+ 
+Good luck and have fun!
---
+++ Day-2/practice.js
@@ -0,0 +1,198 @@
+// const obj = {
+//     print : function(){
+//         function print2(){
+//             console.log(this)
+//         }print2()
+//     }   
+// }
+// obj.print()
+
+
+
+
+
+// const obj ={
+//     name:'bhanu',
+//     fun1 :()=>{
+//         console.log(this)
+//     }
+// }
+// obj.fun1()
+
+
+
+
+
+
+// const obj ={
+//     name:'Bhanu',
+//     fun1:function show(){
+//         console.log(this)
+//     }show()
+    
+// }
+// obj.fun1()
+
+
+
+
+
+
+
+
+
+
+
+// const  obj = {
+//     name:'bhanu',
+//     role:'sde',
+//     print :()=>{
+//         function fun2(){
+//             console.log(this)
+//         }
+//         fun2()
+//     }
+// }
+// obj.print()
+
+// const fun1 = {
+//     name:'bhanu',
+//     role:'sde',
+//     place:'guj'
+// }
+// const fun2 =(fun1 )=>{
+//     const {name ,role , place} = fun1
+//     return([name,
+//     role, 
+//     place])
+// }
+// console.log(fun2(fun1))
+
+
+
+
+
+
+
+// const data = {
+//     name:'bhanu',
+//     role:'sde'
+// }
+// class some{
+//     constructor({name , role}){
+//         this.name = name
+//         this.role = role
+//     }
+//     getitem(){
+//         console.log(this.name)
+//     }
+// }
+// const person = new some(data)
+// person.getitem()
+
+
+
+
+
+
+
+
+// const data = {
+//     a:1,
+//     b:2,
+//     c:3,
+//     d:4,
+//     e:5
+// }
+// class compo{
+//     constructor(data){
+//         Object.assign(this , data)
+//     }
+//     getdata(){
+//         const {...onlyobj} = this
+//         console.log(onlyobj)
+//     }
+// }
+// const alpha =new compo(data)
+// alpha.getdata()
+
+
+
+
+
+
+// const data = [1 ,3,4,[2 ,3 ,[4,5],7,8],9]
+// function flatten(data){
+//         const stack = [...data]
+//         const result = []
+//         while (stack.length){
+//             const next = stack.pop()
+//             Array.isArray(next) ? stack.push(...next):result.push(next)
+//         }
+//         return result.reverse()
+// }
+// console.log(flatten(data))
+
+
+
+
+
+
+// const data = [1 ,3,4,[2 ,3 ,[4,5],7,8],9]
+// function flatten(data){
+//         const stack = [...data]
+//         const result = []
+//         while (stack.length){
+//             const next = stack.shift()
+//             Array.isArray(next) ? stack.unshift(...next):result.push(next)
+//         }
+//         return result
+// }
+// console.log(flatten(data))
+
+
+
+
+
+
+
+// const array = [1 ,3,4,[2 ,3 ,[4,5],7,8],9]
+// let array_2 =[] 
+// const new_array = array.map((item=>{
+       
+//     if(Array.isArray(item)){
+//            array_2 =  array_2.concat(item)
+//         }
+//     else{
+//         array_2.push(item)
+//     }
+//     return array_2
+// }))
+// console.log(array_2)
+
+
+
+
+
+// const data = [1,2,[3,4,[5,6],[6,7] ,8],9]
+// function flatten(data){
+//     return data.reduce((acc , el)=>{
+//         return acc.concat(Array.isArray(el) ?flatten(el) : el)
+//     },[])
+// }
+// console.log(flatten(data))
+
+
+
+// const data = [1,2,3,4,5,6,7]
+// console.log(data.includes(3,2))
+
+
+
+
+// const data = [1,2,3,4,5,6,7,8]
+// console.log(data.join(''))
+
+
+
+
