aaba9ae3187f069df96c7a5bcdb655c155966645
Bhanu-medianv <bhanupratapsingh@medianv>
2025-06-04 14:57:14 +0530

    Exception task

--- Day-9/task03/exception/src/DTO/user.dto.ts
+++ Day-9/task03/exception/src/DTO/user.dto.ts
@@ -1,10 +1,11 @@
-import { IsAlpha, IsNumber, Length, Max , Min, Validate } from "class-validator";
+import { IsAlpha, IsNumber, Length, Matches, Max , Min, Validate } from "class-validator";
 import { Isolder } from "./custom.dto";
 
 
 export class CreateAuthDto{
     @Length(1,40 ,{message:'length should be less then 40'})
     @IsAlpha()
+    @Matches(/^[A-Za-z]+(?:\s[A-Za-z]+)*$/ ,{message : "Name should contain only Alphabets and contains only one space"})
     username:string
     @IsNumber()
     @Min(1,{message:'age must be older than 1'})
