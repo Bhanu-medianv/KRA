a8d064258da4fcf06ce54a963f7947c0399e285e
Bhanu-medianv <bhanupratapsingh@medianv>
2025-06-04 15:05:51 +0530

    class task

--- Day-9/task03/exception/src/DTO/user.dto.ts
+++ Day-9/task03/exception/src/DTO/user.dto.ts
@@ -4,7 +4,7 @@ import { Isolder } from "./custom.dto";
 
 export class CreateAuthDto{
     @Length(1,40 ,{message:'length should be less then 40'})
-    @IsAlpha()
+    
     @Matches(/^[A-Za-z]+(?:\s[A-Za-z]+)*$/ ,{message : "Name should contain only Alphabets and contains only one space"})
     username:string
     @IsNumber()
